edb2b6f4113f5408af0ae5fc1dc32ede85367559
Natsumi.H <natsmy.1211@gmail.com>
2024-06-10 20:38:08 +0800

    microcms client

--- src/lib/client.js
+++ src/lib/client.js
@@ -2,8 +2,8 @@ import { createClient } from "microcms-js-sdk"; //ES6
 
 // Initialize Client SDK.
 export const client = createClient({
-  serviceDomain: "natsumih-blog", // YOUR_DOMAIN is the XXXX part of XXXX.microcms.io
-  apiKey: "9c0dfa1bc16e46c5ac27bda8bc3e2a77a3d8",
+  serviceDomain: process.env.MICROCMS_DOMAIN,
+  apiKey: process.env.MICROCMS_API_KEY,
 });
 
-export const MICROCMS_TOKEN = process.env.X_MICROCMS_API_KEY;
+export const MICROCMS_TOKEN = process.env.MICROCMS_API_KEY;
